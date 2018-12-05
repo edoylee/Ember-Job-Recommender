357fb8ff275227a2a2b7b9d3651570a1d74f3b4f
edoylee <ethandoyle@gmail.com>
2018-12-05 13:48:38 -0800

    Created Ethan Profile

--- predictNew.py
+++ predictNew.py
@@ -7,6 +7,9 @@ from selenium.webdriver import Firefox
 import time
 import random
 import getpass
+from flask import Flask, render_template
+
+app = Flask(__name__, static_url_path="")
 
 CUSTOM_STOP = [
     "a", "about", "above", "across", "after", "afterwards", "again", "against",
@@ -55,32 +58,32 @@ CUSTOM_STOP = [
 
 class LinkedinScraper():
     """ The LinkedinScraper will scrape the linkedin profile of the given name.
-    
+
     Basic Process:
-    
+
         • The find_profile method is used to navigate to the given profile
-        
+
         • The expand_page method is used to find summary, experience, skills, and recommendations
         sections and expand them to be scrapable
-        
-        • The scrape_page method is used to scrape the same four sections if they exist and return 
+
+        • The scrape_page method is used to scrape the same four sections if they exist and return
         empty strings otherwise
-        
+
         • The get_consolidated_profile method will take a dataframe of 4 columns and concatenate them
         into one string to be ready for nlp.
-    
+
     example:
-    
+
         test = LinkedinScraper()
         final_df = test.transform('NAME')
         """
     def __init__(self, param=None):
         self.browser = Firefox()
         self.browser.get('https://www.linkedin.com')
-    
+
     def sleep(self, start=5, end=15):
         return time.sleep(random.randint(start, end))
-    
+
     def go_to_profile(self):
         email = input('Please Enter Email ')
         password = getpass.getpass('Please Enter Password ')
@@ -97,7 +100,7 @@ class LinkedinScraper():
         profile_button = self.browser.find_element_by_class_name('tap-target')
         profile_button.click()
         self.sleep()
-    
+
     def find_profile(self, name):
         search_bar = self.browser.find_element_by_css_selector('#ember41 > input:nth-child(1)')
         search_bar.click()
@@ -110,13 +113,13 @@ class LinkedinScraper():
         self.sleep()
         search_results = self.browser.find_elements_by_class_name('name')
         search_results[0].click()
-        
+
     def scroll(self):
         sections = self.browser.find_elements_by_class_name('pv-profile-section')
         for section in sections:
             section.location_once_scrolled_into_view
             self.sleep()
-        
+
     def expand_page(self):
         try:
             rec = self.browser.find_elements_by_class_name('pv-profile-section__see-more-inline')
@@ -149,7 +152,7 @@ class LinkedinScraper():
                 sleep()
         except:
             pass
-        
+
     def second_expand_page(self):
         summary_section = self.browser.find_element_by_class_name('pv-top-card-section__summary')
         summary_section.location_once_scrolled_into_view
@@ -185,7 +188,7 @@ class LinkedinScraper():
             rec[1].click()
         except:
             pass
-    
+
     def scrape_page(self):
         summary = []
         try:
@@ -199,7 +202,7 @@ class LinkedinScraper():
             experience.append(find_experience.text)
         except:
             experience.append('')
-        education = []    
+        education = []
         try:
             self.sleep()
             find_education = self.browser.find_element_by_class_name('education-section')
@@ -219,32 +222,32 @@ class LinkedinScraper():
         except:
             recommendations.append('')
         profile_df = pd.DataFrame({'summary': summary,
-                                   'experience': experience, 
+                                   'experience': experience,
                                    'education': education,
-                                   'skills': skills, 
+                                   'skills': skills,
                                    'recommendations': recommendations})
         return profile_df
-    
+
     def concat_strings(self, word_list):
         total_cleaned = ''
         for word in word_list:
             total_cleaned += (word + ' ')
         return total_cleaned
-    
+
     def get_consolidated_profile(self, df, columns):
         total_string = ''
         for column in columns:
             total_string += self.concat_strings(df[column])
         return [total_string]
-    
+
     def convert_to_csv(self, df):
         check_variable = input('Proceed? (yes / no)')
         if check_variable == 'yes':
             name = input('Enter name: ')
-            df.to_csv('../data/%s.csv' % name)
+            df.to_csv('~/galvanize/capstone/Ember-Job-Recommender/data/%s.csv' % name)
         else:
             return df
-    
+
     def transform(self):
         self.go_to_profile()
         self.second_expand_page()
@@ -255,72 +258,72 @@ class LinkedinScraper():
 
 class ScrapeGlass():
     """ ScrapeGlass is used to scrape job postings from Glassdoor.
-    
+
     Basic Process:
-    
+
     • The search method will go to the url and search for the query.
-    
+
     • The click_wait method will click the second job posting to
-      trigger the automatic pop-up window, then it will find the 
+      trigger the automatic pop-up window, then it will find the
       x-button in the corner and click it.
-      
-    • The loop_pages method will find the number of pages at the 
-      bottom of the screen use that number in a for loop to iterate 
+
+    • The loop_pages method will find the number of pages at the
+      bottom of the screen use that number in a for loop to iterate
       through all of the pages for the given query.
-      
+
     • The get_job_postings method is called in the loop_pages function
       in order to iterate through every job listing on the page and scrape
       the descriptions. The descriptions are then appended to a class variable.
-      
+
     • Finally the return_job_descriptions method is called to prompt the user
       to decide whether or not to save the scraped results to a csv file.
-    
+
     All of this is being done through the transform method.
-    
+
     example:
-    
+
         url = 'www.glassdoor.com'
         query = 'Data Scientist'
-    
+
         test = ScrapeGlass()
         jobs = test.transform(url, query)
-    
+
     Final Notes:
-    
+
         If the program is interupted for any reason the user will still be prompted
-        on whether or not to proceed with exporting to csv. 
-    
+        on whether or not to proceed with exporting to csv.
+
     """
-    
+
     def __init__(self, param=None):
         self.browser = Firefox()
         self.job_descriptions = []
-    
+
     def click_wait(self):
         """ This method triggers the automatic pop-up window and exits out of it."""
-        
+
         listings = self.browser.find_elements_by_class_name('jl')
         listings[1].click()
         x_button = self.browser.find_element_by_class_name('xBtn')
         x_button.click()
-    
+
     def return_job_descriptions(self):
         """ This method prompts the user whether or not to convert to csv"""
-        
+
         print(len(self.job_descriptions))
         check_variable = input('\nProceed? (yes / no) ')
         if check_variable == 'yes':
             name = input('Enter name of data')
-            self.convert_to_csv(self.job_descriptions, ('../data/%s.csv' % name))
+            self.convert_to_csv(self.job_descriptions, ('~/galvanize/capstone/Ember-Job-Recommender/data/%s.csv' % name))
         else:
             return self.job_descriptions
-    
+
     def sleep(start=5, end=15):
         return time.sleep(random.randint(5, 15))
-        
+
     def search(self, query):
         """ This method goes to the url, and searchs for the query"""
-        
+
         self.browser.get('https://www.glassdoor.com')
         self.sleep()
         keyword_search = self.browser.find_element_by_css_selector('#KeywordSearch')
@@ -328,10 +331,10 @@ class ScrapeGlass():
         keyword_search.send_keys(query)
         start_search = self.browser.find_element_by_css_selector('#HeroSearchButton')
         start_search.click()
-    
+
     def loop_pages(self):
         """ This method iterates through all available pages"""
-        
+
         pages = self.browser.find_elements_by_class_name('page')
         while len(pages) == 5:
             self.get_job_postings()
@@ -339,11 +342,11 @@ class ScrapeGlass():
             next_button.click()
             self.sleep()
         return self.return_job_descriptions()
-    
+
     def get_job_postings(self):
-        """ This method iterates through all of the listings and appends 
+        """ This method iterates through all of the listings and appends
         them to the class variable self.job_descriptions"""
-        
+
         job_listings = self.browser.find_elements_by_class_name('jl')
         self.sleep()
         for job in job_listings:
@@ -355,21 +358,21 @@ class ScrapeGlass():
             choice = random.randint(1,3)
             if choice == 2:
                 tabs = self.browser.find_elements_by_class_name('tabLabel')
-                try:    
+                try:
                     tabs[random.randint(1,2)].click()
                 except IndexError:
                     pass
             self.sleep()
         return self.job_descriptions
-    
+
     def convert_to_csv(self, final_content, name):
-        """ This method converts the list final_content into a pandas 
+        """ This method converts the list final_content into a pandas
         dataframe and adds a 'lables' column of zeros"""
-        
+
         final_txdf = pd.DataFrame({'jobs': final_content})
         final_txdf['labels'] = np.zeros(final_txdf.shape[0])
         final_txdf.to_csv(name)
-    
+
     def transform(self, query):
         """ This method takes the url, and query as inputs and outputs either
         an exported csv file or a list of jobs"""
@@ -382,25 +385,25 @@ class ScrapeGlass():
         self.return_job_descriptions()
 
 class PreprocessData():
-    
+
     def __init__(self, param=None):
         self.param = param
         self.vectorizer = TfidfVectorizer(stop_words=CUSTOM_STOP)
-        
-    
+
+
     def bring_in_data(self, X, y):
         df = pd.read_csv(X, index_col=0)
         df = df.dropna()
         y_vector = pd.read_csv(y, index_col=0)
         full_df = y_vector['profile'].append(df['jobs'])
         return full_df
-    
+
     def get_tfidf(self, df):
         self.vectorizer.fit(df)
         transformed_model = self.vectorizer.transform(df)
         tfidf_df = pd.DataFrame(transformed_model.toarray())
         return tfidf_df
-    
+
     def get_distances(self, df):
         profile = df.iloc[0, :]
         distances = []
@@ -409,7 +412,7 @@ class PreprocessData():
         sorted_distances = np.sort(distances)
         indices = np.argsort(distances)
         return sorted_distances, indices
-    
+
     def get_total_df(self, indices, full_df, sorted_distances, tfidf_df):
         indices_df = pd.DataFrame({'indices': indices})
         sorted_distances_df = pd.DataFrame({'distances': sorted_distances})
@@ -419,7 +422,7 @@ class PreprocessData():
         total_df = pd.concat([indices_df, sorted_df, sorted_distances_df, sorted_tfidf], axis=1)
         total_df.iat[0,2] = 10.0
         return total_df
-    
+
     def transform(self, X, y):
         full_df = self.bring_in_data(X, y)
         tfidf_df = self.get_tfidf(full_df)
@@ -428,10 +431,10 @@ class PreprocessData():
         return total_df
 
 class Predict():
-    
+
     def __init__(self, df):
         self.df = df
-    
+
     def prompt_user(self, df, index):
         recommended_posting = pd.DataFrame(df.iloc[index, :]).T
         original_index = int(recommended_posting['indices'])
@@ -445,7 +448,7 @@ class Predict():
         if label == 'no':
             self.df.iat[label_index,2] = -1.0
         return self.df
-    
+
     def improve_yes(self, df):
         yes_df = pd.DataFrame(df[df['labels'] == 1.0])
         yes_remains = pd.DataFrame(df[df['labels'] == 0.0])
@@ -456,7 +459,7 @@ class Predict():
                 distances_from_yes[j,i] = (spatial.distance.cosine(yes_df.iloc[0,4:], yes_remains.iloc[j,4:]))
         yes_remains['distances_from_yes'] = np.sum(distances_from_yes, axis=1)
         return yes_remains
-    
+
     def improve_no(self, df):
         no_df = pd.DataFrame(df[df['labels'] == -1.0])
         no_remains = pd.DataFrame(df[df['labels'] == 0.0])
@@ -467,7 +470,7 @@ class Predict():
                 distances_from_no[j,i] = (spatial.distance.cosine(no_df.iloc[0,4:], no_remains.iloc[j,4:]))
         no_remains['distances_from_no'] = np.sum(distances_from_no, axis=1)
         return no_remains
-    
+
     def find_next_best(self, df, index=1):
         adjusted_df = self.prompt_user(df, index)
         try:
@@ -480,15 +483,15 @@ class Predict():
         remains['total_distance'] = total_distance
         next_best_index = remains.total_distance.idxmin()
         return self.find_next_best(remains, next_best_index)
-    
+
     def convert_to_csv(self):
         check_variable = input('Proceed? (yes / no)')
         if check_variable == 'yes':
             name = input('Enter name: ')
-            self.df.to_csv('../data/%s.csv' % name)
+            self.df.to_csv('/data/%s.csv' % name)
         else:
             return self.df
-    
+
     def transform(self, df):
         try:
             self.find_next_best(df)
@@ -503,16 +506,16 @@ class PruneForest():
         feature_importances = model.feature_importances_
         idxs = np.nonzero(feature_importances)[0]
         return idxs
-    
+
     def get_reverse_term_dict(self, vectorizer):
         word_dict = vectorizer.vocabulary_
         reverse_dict = {value: key for key, value in word_dict.items()}
         return reverse_dict
-    
+
     def get_word_list(self, reverse_dict, idxs):
         word_list = [reverse_dict[key] for key in list(idxs)]
         return word_list
-    
+
     def get_vocabulary(self, model, vectorizer):
         indices = self.get_feature_importances(model)
         reverse_dict = self.get_reverse_term_dict(vectorizer)
@@ -525,35 +528,35 @@ class FitModel():
         self.forest = RandomForestClassifier(n_estimators=2000, criterion='entropy', n_jobs=-1)
         self.grad_boost = GradientBoostingClassifier(learning_rate=0.001, n_estimators=500, subsample=0.5)
         self.pruned_forest = PruneForest()
-    
+
     def get_Xy(self, name):
         df = pd.read_csv(name, index_col=0)
         culled_df = df[df.labels != 0.0]
         X = culled_df.iloc[1:, 1]
         y = culled_df.iloc[1:, 2]
         return X, y
-    
+
     def fit_primary(self, X, y):
         self.vectorizer.fit(X)
         transformed_matrix = self.vectorizer.transform(X)
         tfidf_df = pd.DataFrame(transformed_matrix.toarray())
         self.forest.fit(tfidf_df, y)
-        return self.forest, self.vectorizer 
-    
+        return self.forest, self.vectorizer
+
     def prune_forest(self, forest, vectorizer):
         vocabulary = self.pruned_forest.get_vocabulary(forest, vectorizer)
         return vocabulary
-    
+
     def improve(self, X, vocabulary):
         imp_vectorizer = TfidfVectorizer(stop_words=CUSTOM_STOP, vocabulary=vocabulary)
         imp_vectorizer.fit(X)
         transformed_matrix = imp_vectorizer.transform(X)
         tfidf_df = pd.DataFrame(transformed_matrix.toarray())
         return tfidf_df
-    
+
     def fit_secondary(self, X, y):
         return self.grad_boost.fit(X, y)
-        
+
     def transform(self, name):
         X, y = self.get_Xy(name)
         forest, vectorizer = self.fit_primary(X, y)
@@ -561,10 +564,23 @@ class FitModel():
         tfidf_df = self.improve(X, vocabulary)
         return self.fit_secondary(tfidf_df, y)
 
+@app.route('/')
+def index():
+    data = PreprocessData()
+    total_df = data.transform('data/dsjobs_training_culled.csv',
+    'data/ethan_profile.csv')
+    print(total_df.iloc[0,1])
+    return render_template('index.html')
+
+@app.route('/handle_yes', methods=['POST'])
+def handle_yes():
+    pass
+
+
 class Main():
     def __init__(self, param=None):
         self.param = param
-    
+
     def transform(self):
         li = LinkedinScraper()
         li.transform()
@@ -583,4 +599,3 @@ class Main():
 
 if __name__ == '__main__':
     pass
-
